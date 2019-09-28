8418849d6fc4c58fa8c2d57c6fe9e5a9cf31bbec
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-09-28 16:22:48 -0600

    Add components to App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
+import Todos from './Todos/Todos';
 
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Dinner with J-Dawg',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Make gnocchi',
+        completed: false
+      }
+    ]
+  }
+
   render() {
     return (
       <section className='App'>
-         <h1>It's working!</h1>
+         <h1>Todo List</h1>
+         <Todos todos={this.state.todos}/>
       </section>
     )
   }
